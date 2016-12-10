ae8be66bd4dde023219b5565fd4d77f13a8b9e1a
Leo-Mahon <leo.mahon@moodinternational.com>
2016-12-10 23:39:35 +0000

    more fire

--- src/FirePlace.hx
+++ src/FirePlace.hx
@@ -1,4 +1,5 @@
 package;
+import com.haxepunk.graphics.Image;
 import com.haxepunk.graphics.Spritemap;
 
 /**
@@ -10,14 +11,20 @@ class FirePlace extends BaseWorldEntity
 
 	public function new() 
 	{
-		var fire = new Spritemap("graphics/fire.png", 64,64);
+		var fire = new Spritemap("graphics/fire.png", 64, 64);
+		var frame = new Image("graphics/fireframe.png");
+		var back = new Image("graphics/fireback.png");
 		
-		fire.add("burn", [0, 1, 2, 3, 4], 10);
+		fire.add("burn", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11], 10);
 		fire.play("burn");
 		
-		this.graphic = fire;
+		this.addGraphic(back);
+		this.addGraphic(fire);
+		this.addGraphic(frame);
 		
 		super();
+		
+		this.setHitbox(64, 64);
 	}
 	
 }
